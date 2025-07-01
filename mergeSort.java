8a6226dc0149a2f15524a45b813b6aa20fd72ecb
Abhijeet R Sharma <97837777+abhii8086@users.noreply.github.com>
2025-07-01 23:50:16 +0530

    Update mergeSort.java

--- mergeSort.java
+++ mergeSort.java
@@ -57,3 +57,59 @@ public class mergeSort {
 	}
 
 }
+////////////////////////////////////// new code
+
+package sorting;
+
+public class mergeSort {
+
+	public static void main(String[] args) {
+		int arr[] = { 5, 4, 1, 3, 2 };
+		int n = arr.length;
+		int low = 0;
+		int high = n - 1;
+
+		ms(arr, low, high);
+
+		for (int x : arr)
+			System.out.print(" " + x);
+	}
+
+	public static void ms(int arr[], int low, int high) {
+
+		if (low >= high)
+			return;
+		int mid = low + (high - low) / 2;
+		ms(arr, low, mid);
+		ms(arr, mid + 1, high);
+		mrg(arr, low, mid, high);
+
+	}
+
+	public static void mrg(int arr[], int low, int mid, int high) {
+		int temp[] = new int[high - low + 1];
+		int left = low;
+		int right = mid + 1;
+		int k = 0;
+
+		while (left <= mid && right <= high) {
+			if (arr[left] < arr[right]) {
+				temp[k++] = arr[left++];
+			} else {
+				temp[k++] = arr[right++];
+			}
+		}
+		while (left <= mid) {
+			temp[k++] = arr[left++];
+		}
+		while (right <= high) {
+			temp[k++] = arr[right++];
+		}
+
+		for (int i = 0; i < temp.length; i++) {
+			arr[low + i] = temp[i];
+		}
+
+	}
+
+}
